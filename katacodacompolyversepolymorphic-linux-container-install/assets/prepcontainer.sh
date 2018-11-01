f9254c0c5627e15e8919e92752b38a3732d99a3c
Archis Gore <archis@polyverse.io>
2018-11-01 08:28:04 -0700

    Updated

--- katacodacompolyversepolymorphic-linux-container-install/assets/prepcontainer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-apt-get update && apt-get install -y bash
-while true ; do script -q -c "/bin/bash -l" /dev/null ; done
--- katacodacompolyversepolymorphic-linux-container-install/jump-to-ubuntu-container.sh
+++ katacodacompolyversepolymorphic-linux-container-install/jump-to-ubuntu-container.sh
@@ -1,2 +1,7 @@
 touch ~/prepintro
-docker run --rm -it -v ~/scripts:/scripts --entrypoint=/scripts/prepcontainer.sh ubuntu
+
+# !/bin/sh
+# apt-get update && apt-get install -y bash
+# while true ; do script -q -c "/bin/bash -l" /dev/null ; done
+
+docker run --rm -it ubuntu sh
