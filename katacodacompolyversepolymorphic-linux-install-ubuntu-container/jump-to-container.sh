ed4741600ea3c1130dd7ce59d933844d41382a58
Archis Gore <archis@polyverse.io>
2018-11-06 14:58:53 -0800

    Updated

--- katacodacompolyversepolymorphic-linux-install-ubuntu-container/jump-to-container.sh
+++ katacodacompolyversepolymorphic-linux-install-ubuntu-container/jump-to-container.sh
@@ -3,9 +3,10 @@
 cat << EOF > /tmp/noexit.sh
 !/bin/sh
 apt-get update && apt-get install -y bash
+clear
 while true ; do script -q -c "/bin/bash -l" /dev/null ; done
 EOF
 
 chmod a+x /tmp/noexit.sh
 
-docker run --rm -it -v /tmp/noexit.sh:/noexit.sh ubuntu /noexit.sh
+docker run --rm -it -v /tmp/noexit.sh:/noexit.sh ubuntu /bin/sh /noexit.sh
