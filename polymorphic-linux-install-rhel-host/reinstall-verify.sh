cd7a5b6471c24c9bd20d3868a35ce631f3b05188
Archis Gore <archis@polyverse.io>
2019-01-07 12:52:02 -0800

    Updated verify scripts for RHEL

--- polymorphic-linux-install-rhel-host/reinstall-verify.sh
+++ polymorphic-linux-install-rhel-host/reinstall-verify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[ "$(dpkg --get-selections | awk '{print $1}' | xargs apt-cache madison | grep -i polyverse)" != "" ] && echo "done"
+[ "$(docker exec demo /usr/bin/rpm -qa vendor="*Polyverse*")" != "" ] && echo "done"
