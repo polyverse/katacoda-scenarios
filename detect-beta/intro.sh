5bc6d76665fb91e0dd10b57a98247243ec1479cf
Archis Gore <archis@polyverse.com>
2020-04-25 14:03:42 -0700

    Added polytect installation in katacoda detect demo

--- detect-beta/intro.sh
+++ detect-beta/intro.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+echo "Starting dashboard..."
 docker run -p 8080:8080 -d --rm polyverse/detect-demo-dashboard
+
+echo "Installing Polytect..."
+curl -L https://github.com/polyverse/polytect/releases/latest/download/install.sh | sudo sh -s "xU9XiObcoKnSNpRhpuwt5Lrem" "katacoda_detect_scenario"
