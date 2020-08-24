9ad106ec4ba57c1eea2bd317186760295586b261
Archis Gore <archis@polyverse.com>
2020-08-24 13:05:34 -0700

    Use PWD where mtd.ko is build

--- environments/brop/build/3_installmtdmodule.sh
+++ environments/brop/build/3_installmtdmodule.sh
@@ -2,4 +2,4 @@ git clone https://github.com/polyverse/mtd-module.git
 cd mtd-module/src
 make ubuntu
 sudo insmod mtd.ko
-echo "install mtd insmod /mtd-module/src/mtd.ko" > /etc/modprobe.d/mtd.conf
+echo "install mtd insmod $PWD/mtd.ko" > /etc/modprobe.d/mtd.conf
