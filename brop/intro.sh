15d2e394944bd8d12c4d85435649154825906b2b
Archis Gore <archis@polyverse.com>
2020-08-25 18:47:57 -0700

    Enable the exception traces used for analytics

--- brop/intro.sh
+++ brop/intro.sh
@@ -1,2 +1,8 @@
 # Insert mtd module (precompiled in environment)
 insmod /root/mtd-module/src/mtd.ko
+
+# Track kernel traps (which are used for analytics)
+echo "1" >/proc/sys/debug/exception-trace
+
+# Ignore fatal signals which are not
+echo "0" >/proc/sys/kernel/print-fatal-signals
