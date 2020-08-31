3cb9808e6a18ba6c3354e4dcffdd5c7981aaff25
Archis Gore <archis@polyverse.com>
2020-08-31 14:20:37 -0700

    Add polycorder key and node_id so we can capture logs upstream.

--- brop/intro.sh
+++ brop/intro.sh
@@ -6,3 +6,16 @@ echo "1" >/proc/sys/debug/exception-trace
 
 # Ignore fatal signals which are not
 echo "1" >/proc/sys/kernel/print-fatal-signals
+
+# Randomized node id
+node_id=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+# Add polycorder key and randomized nodeid to polycorder config
+echo "" >> /etc/zerotect/zerotect.toml
+echo "[polycorder_config]" >> /etc/zerotect/zerotect.toml
+echo "auth_key = 'xU9XiObcoKnSNpRhpuwt5Lrem'" >> /etc/zerotect/zerotect.toml
+echo "node_id = '$node_id'" >> /etc/zerotect/zerotect.toml
+
+# Restart zerotect to pick up polycorder config
+systemctl restart zerotect
+
