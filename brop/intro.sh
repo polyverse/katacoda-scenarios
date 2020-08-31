cdccbf6992fc7bae619f4afcb7e18ee9a0a837da
Archis Gore <archis@polyverse.com>
2020-08-31 16:20:19 -0700

    Abandon tinyproxy experiment

--- brop/intro.sh
+++ brop/intro.sh
@@ -19,30 +19,5 @@ echo "node_id = '$node_id'" >> /etc/zerotect/zerotect.toml
 # Restart zerotect to pick up polycorder config
 systemctl restart zerotect
 
-# Tell tinyproxy to proxy the graph on localhost
-cat <<tinyconf >/etc/tinyproxy/tinyproxy.conf
-User tinyproxy
-Group tinyproxy
-Port 8888
-DefaultErrorFile "/usr/share/tinyproxy/default.html"
-StatFile "/usr/share/tinyproxy/stats.html"
-Logfile "/var/log/tinyproxy/tinyproxy.log"
-LogLevel Info
-PidFile "/run/tinyproxy/tinyproxy.pid"
-MaxClients 10
-MinSpareServers 1
-MaxSpareServers 2
-StartServers 2
-MaxRequestsPerChild 0
-ViaProxyName "tinyproxy"
-
-ConnectPort 443
-ConnectPort 563
-
-ReverseOnly Yes
-ReverseMagic Yes
-
-ReversePath "/" "https://polycorder.polyverse.com/v1/events/cwgraph?authorization=QP3t2Nyv3khbq6HEhkkuE0Ccr&pastHours=1&org=false&global=false&nodeId=$node_id"
-
-tinyconf
+#"http://polycorder.polyverse.com/v1/events/cwgraph?authorization=QP3t2Nyv3khbq6HEhkkuE0Ccr&pastHours=1&org=false&global=false&nodeId=$node_id"
 
--- environments/brop/build/1_installpackages.sh
+++ environments/brop/build/1_installpackages.sh
@@ -1,2 +1,2 @@
 apt-get -y update && apt-get -y upgrade
-apt-get install -y wget curl vim jq bash git tinyproxy
+apt-get install -y wget curl vim jq bash git
